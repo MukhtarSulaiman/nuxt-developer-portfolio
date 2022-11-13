f5a159c7103a12420d520f5884c7b11de1540843
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-11-13 23:23:36 +0100

    Added transition

--- app.vue
+++ app.vue
@@ -5,7 +5,23 @@
 </template>
 
 <style lang="scss">
+     
      body {
           font-family: 'Raleway', sans-serif;
+     }
+
+     * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+     }
+
+     a {
+          color: $color-text-primary;
+          text-decoration: none;
+     }
+
+     ul {
+          list-style: none;
      }
 </style>
--- components/Header.vue
+++ components/Header.vue
@@ -2,19 +2,19 @@
 
 <script setup>
 
-     let navOpen = ref(true);
+     const navOpen = ref(false);
 
      onMounted(() => {
-          window.addEventListener('resize', handleResize);
-          handleResize();
+          window.addEventListener('resize', toggleNavbar);
+          toggleNavbar();
      });
 
      onUnmounted(() => {
-          window.removeEventListener('resize', handleResize);
+          window.removeEventListener('resize', toggleNavbar);
      });
 
-     function handleResize() {
-          window.innerWidth <= 1024 ? navOpen.value = false : navOpen.value = true;
+     function toggleNavbar() {
+          window.innerWidth >= 1024 ? navOpen.value = true : navOpen.value = false;
      }
 
 
@@ -34,25 +34,25 @@
                :class="{active: !navOpen}"
                class="navbar-toggler"
                type="button"
-               aria-label="Toggle navigation">
+               aria-label="toggle navigation">
                <span class="navbar-toggler__bar navbar-toggler__bar--top"></span>
                <span class="navbar-toggler__bar navbar-toggler__bar--middle"></span>
                <span class="navbar-toggler__bar navbar-toggler__bar--bottom"></span>
           </button>
-          <nav class="" id="navbarNav" v-show="navOpen">
+          <nav  :class="{ active: navOpen }" id="navbarNav">
                <ul>
                     <li><NuxtLink to="#intro">Accueil</NuxtLink></li>
                     <li><NuxtLink to="#skills">Compétences</NuxtLink></li>
                     <li><NuxtLink to="#portfolio">Portfolio</NuxtLink></li>
                     <li><NuxtLink to="#education">Formations</NuxtLink></li>
                     <li><NuxtLink to="#contact">Contact</NuxtLink></li>
-                    <li>
-                         <span>
+                    <li class="nav-item nav-item-mode-switch me-4">
+                         <span class="dark-mode-toggle fs-3" role="button">
                               <!-- <Icon name="ic:outline-wb-sunny" /> -->
                               <Icon name="ic:baseline-bedtime" id="moon" />
                          </span>
                     </li>
-                    <li>
+                    <li class="nav-item nav-item-lang">
                          <select id="site-language" name="language">
                               <option value="French">FR</option>
                               <option value="English">EN</option>
@@ -70,10 +70,11 @@
           width: 100%;
           height: 60px;
           padding: 1.2rem;
+          position: relative;
           z-index: 10;
           color: $color-text-primary;
           background-color: $color-brand-tertiary;
-
+     
           nav {
                position: absolute;
                top: 60px;
@@ -81,21 +82,26 @@
                width: 100%;
                height: 92vh;
                background-color: $color-brand-tertiary;
+               transition: all .7s;
+               transform: translateX(100%);
+
+                &.active {
+                    display: block;
+                    transform: translateX(0%);
+               }
+               
                ul {
                     height: inherit;
-                    @include flexbox(
-                         $justify-content: space-around,
-                         $flex-direction: column
-                    );
+                    @include flexbox($justify-content: space-around,$flex-direction: column);
                     
                     li {
                          margin: 0.6rem;
-
+                         // opacity: 0;
                          .router-link-active {
                               color: rgb($color-text-primary, 0.7) !important;
                               font-size: 1.3rem;
                          }
-                    }
+                    }            
                }
           }
                
@@ -107,9 +113,9 @@
                position: relative;
                border: 0;
                background: none;
+               cursor: pointer;
         
                &__bar {
-                    display: block !important;
                     width: 100%;
                     height: 1.5px;
                     position: absolute;
@@ -133,25 +139,25 @@
                          transform: rotate(-135deg);
                     }
                }
-          }
 
-          // State when the navbar is collapsed
-          .active {
-               .navbar-toggler__bar {
-                    &--top {
-                         width: 110%;
-                         margin-top: -20px;
-                         transform: rotate(0deg);
-                    }
-                    &--middle {
-                         width: 70%;
-                         opacity: 1;
-                         filter: alpha(opacity=100);
-                    }
-                    &--bottom {
-                         width: 95%;
-                         margin-top: 20px;
-                         transform: rotate(0deg);
+                // State when the navbar is collapsed
+               &.active {
+                    .navbar-toggler__bar {
+                         &--top {
+                              width: 110%;
+                              margin-top: -20px;
+                              transform: rotate(0deg);
+                         }
+                         &--middle {
+                              width: 70%;
+                              opacity: 1;
+                              filter: alpha(opacity=100);
+                         }
+                         &--bottom {
+                              width: 95%;
+                              margin-top: 20px;
+                              transform: rotate(0deg);
+                         }
                     }
                }
           }
@@ -169,10 +175,10 @@
                     position:static !important;
                     width: auto !important;
                     height: initial !important;
+                    transform: translateX(0%) !important;
                }
                ul {
-                    @include flexbox($justify-content: space-between,
-                         $flex-direction: row !important);
+                    @include flexbox($justify-content: space-between, $flex-direction: row !important);
                }
                 .navbar-toggler {
                     display: none !important;
