009d66a2822662c5bb1c1b7774e049089a675feb
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-12-03 23:53:51 +0100

    Added language orintation

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<!-- @format -->
+
+<script setup lang="ts">
+     import { useI18n } from 'vue-i18n';
+     
+     const { locale, t} = useI18n();
+
+     function changLangDirection() {
+          locale.value === 'ar' ?
+               document.body.style.direction = 'rtl'
+               : document.body.style.direction = 'ltr';
+     }
+
+     watch(locale, () => {
+          changLangDirection();
+     });
+
+</script>
+
 <template>
      <NuxtLayout>
           <NuxtPage />
@@ -5,30 +24,31 @@
 </template>
 
 <style lang="scss">
-
      :root {
-	--color-page-background: #{$color-brand-tertiary};
-	--color-text-icon: #{$color-text-primary};
+          --color-page-background: #{$color-brand-tertiary};
+          --color-text-icon: #{$color-text-primary};
      }
 
      .dark-mode,
-     .dark-mode :is(i) {
+     .dark-mode :is(i, .dynamic-description) {
           @include color-mode(
-               var(--color-text-icon), var(--color-page-background)
+               var(--color-text-icon),
+               var(--color-page-background)
           );
      }
 
      .light-mode,
-     .light-mode :is(i) {
-         @include color-mode(
-               var(--color-page-background), var(--color-text-icon)
+     .light-mode :is(i, .dynamic-description) {
+          @include color-mode(
+               var(--color-page-background),
+               var(--color-text-icon)
           );
      }
 
      html {
-          transition: all .7s;
+          transition: all 0.7s;
      }
-     
+
      body {
           font-family: 'Raleway', sans-serif;
      }
@@ -39,11 +59,22 @@
           box-sizing: border-box;
      }
 
+     main {
+          margin: 0 1.2rem;
+          // overflow: hidden;
+     }
+
      a {
           text-decoration: none;
      }
 
      ul {
           list-style: none;
+     }
+
+     @media only screen and (min-width: 640px) {
+          main {
+               margin: 0 4rem;
+          }
      }
 </style>
