2506eba55db8c5bf612b12c11fa70a21fbd65bb9
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-12-04 23:26:39 +0100

    Added & styled hero section

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,13 +1,203 @@
-<script setup lang="ts">
+<!-- @format -->
 
+<script setup lang="ts">
+	const isAdditionalDescActive = ref(false);
 </script>
 
 <template>
-   <section id="hero" class="hero-section">
-        <SocialNetwork />
-    </section>
+	<section>
+		<div class="hero-section hero-section__container">
+			<SocialNetwork />
+			<div class="hero-section__intro">
+				<h1 class="hero-section__main-heading">
+						<span>{{ $t('main_heading.static_desc', 0) }}</span>{{ $t('main_heading.static_desc', 1) }}
+						<span>{{ $t('main_heading.static_desc', 2) }}</span>
+						<span>
+							<span class="static-description">{{$t('main_heading.dynamic_desc', 1)}}</span>
+							<span class="dynamic-description">{{$t('main_heading.dynamic_desc', 2)}}</span>
+						</span>
+				</h1>
+				<div :class="['hero-section__description-container', {active: isAdditionalDescActive}]">
+					<p class="hero-section__main-description">{{ $t('description_info.main') }}</p>
+					<p class="hero-section__additional-description">
+							<span class="text-primary fs-4 fw-bold">{{$t('description_info.additional', 1)}}</span>
+							{{ $t('description_info.additional', 2) }}
+					</p>
+					<button @click="(isAdditionalDescActive = !isAdditionalDescActive)"  class="hero-section__btn-more-info" type="button"> 
+						{{ isAdditionalDescActive ? $t('description_info.button', 0) : $t('description_info.button', 1) }}
+						<i>
+							<Icon :name="isAdditionalDescActive ? 'ic:baseline-arrow-back-ios' : 'ic:baseline-arrow-forward-ios'"/>
+						</i>
+					</button>
+				</div>
+			</div>
+			<div class="hero-section__hero-img ">
+				<img src="~/assets/images/backgrounds/personal-image.png" :alt="$t('hero_img_desc')" />
+			</div>
+		</div>
+	</section>
 </template>
 
 <style lang="scss" scoped>
 
-</style>v
+	section {
+		margin-top: 7rem;
+		text-align: center;
+	}
+	.hero-section {
+		
+		&__container {
+			@include flexbox($flex-direction: column);
+		}
+
+		&__intro {
+			order: 2;
+		}
+
+		&__hero-img {
+			order: 1;
+			img {
+				width: 85%;
+				max-width: 600px;
+			}
+		}
+
+		&__main-heading {
+			margin-top: 1.5rem;
+			margin-bottom: 3rem;
+			font-weight: 500;
+
+			span {
+				display: block;
+			}
+
+			span:nth-child(1) {
+				font-size: 1.2rem;
+				opacity: 0.7;
+			}
+
+			span:nth-child(2) {
+				display: inline;
+				font-weight: 700;
+				color: $color-brand-primary;
+			}
+
+			span:nth-child(3) {
+				padding-top: 2rem;
+				font-size: 1.3rem;
+				font-weight: 500;
+			}
+
+			span:nth-child(3) .static-description {
+				margin-right: 5px;
+			}
+
+			span:nth-child(3) .dynamic-description {
+				position: relative;
+				color: $color-brand-secondary;
+				&::before {
+						content: '';
+						position: absolute;
+						top: 0;
+						left: 0;
+						width: 100%;
+						height: 100%;
+						padding-left: 5px;
+						font-size: 2rem;
+						z-index: 1;
+						background-color: inherit;
+						border-left: 2px solid $color-brand-secondary;
+						animation: steps(15) infinite;
+						@include flexbox(left);
+						@include animation(4s) {
+							40%,
+							60% {
+								left: calc(100% + 5px);
+							}
+							100% {
+								left: 0;
+							}
+						}
+				}
+			}
+		}
+
+		&__description-container {
+			text-align: initial;
+		}
+
+		&__main-description {
+			font-weight: 400;
+			word-spacing: 2px;
+			opacity: 0.8;
+
+		}
+
+		&__additional-description {
+			max-height: 0;
+			margin-top: 2rem;
+			font-size: 0.9rem;
+			font-weight: 200;
+			word-spacing: 3px;
+			opacity: 0;
+			transition: all .6s;
+
+			span:nth-child(1) {
+				color: $color-brand-primary;
+				font-size: 1.4rem;
+				font-weight: 700;
+			}
+		}
+
+		&__description-container.active &__additional-description {
+			opacity: 0.8 ;
+			max-height: fit-content;
+		}
+
+		&__btn-more-info {
+			@include custom-btn(161px);
+			border: none;
+			font-size: 1rem;
+			font-weight: 300;
+			margin-top: 1.5rem;
+			transform: translateY(0px);
+			transition: transform .6s;
+			
+			i {
+				color: var(--color-text-icon);
+				background-color: transparent;
+			}
+		}
+
+		@media screen and (min-width: 1024px) {
+			margin-top: 9rem;
+			&__container {
+				width: fit-content;
+				height: 500px;
+				text-align: initial;
+				@include flexbox(center, flex-start, $flex-direction: row);
+			}
+
+			&__main-heading {
+				font-size: 2rem;
+
+				span:nth-child(3) :nth-child(1) {
+						display: inline !important;
+				}
+			}
+
+			&__intro {
+				order: 1;
+				width: 50%;
+				margin-left: 5rem;
+			}
+
+			&__hero-img {
+				order: 2;
+				width: 50%;
+				height: inherit;
+				@include flexbox(flex-end);
+			}
+		}
+	}
+</style>
--- components/SocialNetwork.vue
+++ components/SocialNetwork.vue
@@ -31,12 +31,58 @@
         <div v-for="network in socialNetworks" :key="network.id" class="network-container__icon-wrapper">
             <NuxtLink :to="network.link" target="_blank"
                 :aria-label="$t('network_aria_label', { iconName: network.icon })">
-                <Icon :name="'mdi:' + network.icon" aria-hidden="true"/>
+                <i class=" dark-mode light-mode"><Icon :name="'mdi:' + network.icon" aria-hidden="true"/></i>
             </NuxtLink>
         </div>
     </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
+    .network-container {
+        display: none;
+
+        @media screen and (min-width: 1024px) {
+            @include flexbox($flex-direction: column);
+            @include contact-and-network-icon(7px);
+            height: inherit;
+            position: relative;
+            // opacity: 0;
+            // @include animation(2s, 1.7s) {
+            // 	0% {
+            // 		opacity: 0;
+            // 		transform: translateX(-118px);
+            // 	}
+            // 	20% {
+            // 		opacity: 0.5;
+            // 	}
+            // 	100% {
+            // 		opacity: 1;
+            // 		transform: translateX(0px);
+            // 	}
+            // }
+            // display: initial;
+
+            &::before,
+            &::after {
+                content: '';
+                width: 2px;
+                height: 130px;
+                position: absolute;
+                @include gradient;
+            }
+
+            &::before {
+                top: 0px;
+            }
+
+            &::after {
+                bottom: 0px;
+            }
+
+            &__icon-wrapper {
+                margin: .6rem 0;
+            }
+        }
+	}
 </style>
