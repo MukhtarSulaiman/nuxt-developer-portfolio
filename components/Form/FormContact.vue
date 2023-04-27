de934847a1707a869905143fa7ed21e6e2379fe5
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-04-27 23:28:24 +0200

    A loader has been added

--- components/Form/FormContact.vue
+++ components/Form/FormContact.vue
@@ -11,6 +11,7 @@
     ]
 
     const sendFormContact = (formData: FormContact) => {
+
         return useFetch('/api/sendmail', {
             headers: {
                 'Content-type': 'application/json'
@@ -23,9 +24,12 @@
     }
 
     const handleSubmit = async (values: FormContact, actions: any): Promise<void> => {
+        const form = document.querySelector<HTMLElement>('form');
+        form.firstElementChild.classList.add('loader');
+
         const { data } = await sendFormContact(values);
-        console.log(data.value)
-        actions.resetForm()
+        actions.resetForm();
+        form.firstElementChild.classList.remove('loader');
     }
 
     configure({
@@ -54,6 +58,7 @@
         v-slot="{ meta: formMeta, errors: formErrors }"
         novalidate
     >
+        <Loader />
         <FormInput
             formGroupClass="form-group"
             v-for="input in inputs"
@@ -105,36 +110,6 @@
             border-right: 2px solid;
             padding-top: 3rem;
         }
-        // div:nth-child(1).loader {
-        //     width: 100%;
-        //     height: 70%;
-        //     position: absolute;
-        //     top: 0;
-        //     left: 0;
-        //     z-index: 2;
-        //     opacity: 0.9;
-        //     z-index: 1;
-        //     @include flexbox;
-        //     &::after {
-        //         content: "";
-        //         width: 70px;
-        //         height: 70px;
-        //         border-radius: 50%;
-        //         border: 5px solid transparent;
-        //         border-top-color: $color-brand-secondary;
-        //         border-bottom-color: $color-brand-secondary;
-        //         z-index: 2;
-        //         animation-iteration-count: infinite !important;
-        //         @include animation(1.5s) {
-        //             0% {
-        //                 transform: rotate(0deg);
-        //             }
-        //             100% {
-        //                 transform: rotate(360deg);
-        //             }
-        //         }
-        //     }
-        // }
         button {
             @include custom-btn(100px);
             height: 40px !important;
---
+++ components/Loader.vue
@@ -0,0 +1,39 @@
+
+<script setup lang="ts"></script>
+
+<template>
+    <div></div>
+</template>
+
+<style lang="scss" scoped>
+      div.loader {
+            width: 100%;
+            height: 70%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 2;
+            opacity: 0.9;
+            z-index: 1;
+            @include flexbox;
+            &::after {
+                content: "";
+                width: 70px;
+                height: 70px;
+                border-radius: 50%;
+                border: 5px solid transparent;
+                border-top-color: $color-brand-secondary;
+                border-bottom-color: $color-brand-secondary;
+                z-index: 2;
+                animation-iteration-count: infinite !important;
+                @include animation(1.5s) {
+                    0% {
+                        transform: rotate(0deg);
+                    }
+                    100% {
+                        transform: rotate(360deg);
+                    }
+                }
+            }
+        }
+</style>
